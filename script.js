cb59daf959d0a15e98c5a1b5d36680c9b72d9df6
Abdul Baqi <abdulbaqiq88@gmail.com>
2024-10-22 11:53:48 +0500

    initial commit

---
+++ script.js
@@ -0,0 +1,8 @@
+// Define the roll number
+const rollNumber = "21SW39";
+
+// Get the element to display the roll number
+const rollNumberElement = document.getElementById("roll-number");
+
+// Display the roll number
+rollNumberElement.textContent = "My Roll Number: " + rollNumber;
